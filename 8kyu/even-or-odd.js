b3af9c0efbb84634d540b1ece6de70e6b857b28a
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-07-09 12:57:16 -0400

    new codewars challenge

--- 8kyu/even-or-odd.js
+++ 8kyu/even-or-odd.js
@@ -2,11 +2,11 @@
 
 function even_or_odd(number) {
 //Solution 1 
-    // if(number % 2 === 0){
-    //     return 'Even'
-    // }else {
-    //     return 'Odd'
-    // }
+    if(number % 2 === 0){
+        return 'Even'
+    }else {
+        return 'Odd'
+    }
 //Solution 2 
     return number % 2 === 0 ? 'Even' : 'Odd'
 
@@ -31,6 +31,5 @@ console.log(even_or_odd(2))
 console.log(even_or_odd(7))
 console.log(even_or_odd(-42))
 console.log(even_or_odd(-7))
-console.log(even_or_odd(0))
 
 
